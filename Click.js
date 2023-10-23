d1b773b5fbb836313ee716d2cd4f0f3c91766778
Annabell <annabellrambla@gmail.com>
2023-10-22 20:19:06 -0400

    Everything works

--- Click.js
+++ Click.js
@@ -1,1 +1,23 @@
+
+const fooButton = document.getElementById('foo');
+const barButton = document.getElementById('bar');
+const foobarButton = document.getElementById('foobar');
 const main = document.querySelector('main');
+
+fooButton.addEventListener('click', function (){
+    const h3 = document.createElement ('h3');
+    h3.textContent = 'Foo';
+    main.appendChild(h3);
+});
+
+barButton.addEventListener('click', function (){
+    const h3 = document.createElement ('h3');
+    h3.textContent = 'Bar';
+    main.appendChild(h3);
+});
+
+foobarButton.addEventListener('click', function (){
+    const h2 = document.createElement ('h2');
+    h2.textContent = 'FooBar';
+    main.appendChild(h2);
+});
